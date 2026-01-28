8271c68411126486161a111a6e5838542f8b8546
thiwankahew <thiwankahewavitharana@gmail.com>
2026-01-28 10:38:04 -0500

    yaml files loading correctly

--- src/bench_robot/launch/robot.launch.py
+++ src/bench_robot/launch/robot.launch.py
@@ -1,7 +1,16 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
 
 def generate_launch_description():
+    pkg_share = get_package_share_directory('bench_robot')
+
+    motor_params = os.path.join(pkg_share,'config','motor_controller.yaml')
+    motor_mux_params = os.path.join(pkg_share,'config','motor_control_mux.yaml')
+
+
     return LaunchDescription([
         Node(
             package='rosbridge_server',
@@ -10,13 +19,13 @@ def generate_launch_description():
         Node(
             package='bench_robot',
             executable='arduino_bridge',
-            parameters=["/home/thwia/CEArobot/src/bench_robot/Config/motor_controller.yaml"],
+            parameters=[motor_params],
             output='screen',
         ),
         Node(
             package='bench_robot',
             executable='motor_control_mux',
-            parameters=["/home/thwia/CEArobot/src/bench_robot/Config/motor_control_mux.yaml"],
+            parameters=[motor_mux_params],
             output='screen',
         ),
         Node(
