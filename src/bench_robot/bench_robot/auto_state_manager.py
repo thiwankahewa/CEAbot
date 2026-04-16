64331955972278307bc8d2e3664cd81eb0eec27b
thiwankahewa <thiwankahewavitharana@gmail.com>
2026-04-16 10:01:16 -0400

    remove unnecosory lines

--- src/bench_robot/bench_robot/auto_state_manager.py
+++ src/bench_robot/bench_robot/auto_state_manager.py
@@ -10,13 +10,10 @@ class AutoStateManager(Node):
     def __init__(self):
         super().__init__("auto_state_manager")
 
-        qos_latched = QoSProfile(
-            depth=1,
-            durability=DurabilityPolicy.TRANSIENT_LOCAL,
-            reliability=ReliabilityPolicy.RELIABLE,
-        )
+        # -------- States and variables --------
+        qos_latched = QoSProfile(depth=1,durability=DurabilityPolicy.TRANSIENT_LOCAL,reliability=ReliabilityPolicy.RELIABLE,)
 
-        # --- Allowed states ---
+        # Allowed states 
         self.valid_states = {
             "idle",
             "bench_tracking_f",
@@ -26,10 +23,9 @@ class AutoStateManager(Node):
             "scan_start",
             "aruco_centering"
         }
-
+        # Allowed transitions 
         self.allowed = {
             "idle": {"align_center", "bench_tracking_f", "bench_tracking_b", "yaw_correction","steer_0", "steer_90"},
-            
             "bench_tracking_f": {"yaw_correction", "idle",  "bench_tracking_b", "align_center"},
             "bench_tracking_b": {"yaw_correction", "idle", "bench_tracking_f", "align_center"},
             "yaw_correction": {"align_center", "idle"},
@@ -41,20 +37,47 @@ class AutoStateManager(Node):
         self.mode = "manual"
         self.auto_state = "idle"
 
-        # Subscribers
+        # -------- subs --------
         self.sub_mode = self.create_subscription(String, "/mode", self.cb_mode, 10)
         self.sub_cmd = self.create_subscription(String, "/auto_state_cmd", self.cb_state_cmd, 10)
 
-        # Publishers
+        # -------- pubs --------
         self.pub_state = self.create_publisher(String, "/auto_state", qos_latched)
 
         # Publish initial state once
         self.publish_state()
 
+    # -------- helper functions --------
+
     def publish_state(self):
         msg = String()
         msg.data = self.auto_state
         self.pub_state.publish(msg)
+    
+    # ---------- Callbacks ----------
+
+    def cb_mode(self, msg: String):
+        m = (msg.data or "").strip().lower()
+        if m != self.mode:
+            self.mode = m
+        self.get_logger().info(f"Robot Mode changed -> {self.mode}")
+
+        # If leaving AUTO, force state to idle (global safety)
+        if self.mode != "auto":
+            self.set_state("idle", reason="mode!=auto (forced)")
+
+    def cb_state_cmd(self, msg: String):
+        cmd = (msg.data or "").strip().lower()
+        if not cmd:
+            return
+
+        if self.mode != "auto":
+            self.get_logger().warning(f"Ignoring auto_state_cmd='{cmd}' because mode='{self.mode}'")
+            return
+
+        self.set_state(cmd, reason="cmd")
+
+    # -------- main function --------
 
     def set_state(self, new_state: str, reason: str = "") -> bool:
         new_state = (new_state or "").strip().lower()
@@ -81,31 +104,6 @@ class AutoStateManager(Node):
         self.get_logger().info(f"auto_state -> {self.auto_state}" + (f" ({reason})" if reason else ""))
         return True
 
-    # ---------- Callbacks ----------
-    def cb_mode(self, msg: String):
-        m = (msg.data or "").strip().lower()
-        if m == self.mode:
-            return
-
-        self.mode = m
-        self.get_logger().info(f"Robot Mode changed -> {self.mode}")
-
-        # If leaving AUTO, force state to idle (global safety)
-        if self.mode != "auto":
-            self.set_state("idle", reason="mode!=auto (forced)")
-
-    def cb_state_cmd(self, msg: String):
-        cmd = (msg.data or "").strip().lower()
-        if not cmd:
-            return
-
-        if self.mode != "auto":
-            self.get_logger().warning(f"Ignoring auto_state_cmd='{cmd}' because mode='{self.mode}'")
-            return
-
-        self.set_state(cmd, reason="cmd")
-
-
 def main(args=None):
     rclpy.init(args=args)
     node = AutoStateManager()
@@ -117,7 +115,6 @@ def main(args=None):
         node.destroy_node()
         if rclpy.ok():
             rclpy.shutdown()
-
 
 if __name__ == "__main__":
     main()
